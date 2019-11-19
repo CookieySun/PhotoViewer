9e97caab6bc104601c1e193fe383c28acc5b63fe
kukita <ku.k669500@gmail.com>
2019-11-19 10:50:15 +0900

    フラグメントをリプレース

--- app/src/main/java/kktyu/xyz/testphotoviewer/MainActivity.kt
+++ app/src/main/java/kktyu/xyz/testphotoviewer/MainActivity.kt
@@ -2,11 +2,17 @@ package kktyu.xyz.testphotoviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.container,
+            PhotoListFragment()
+        ).commit()
     }
 }
