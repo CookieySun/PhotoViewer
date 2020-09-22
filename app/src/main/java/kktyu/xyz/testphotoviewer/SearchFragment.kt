694a78829cca69cc7848de2ac9a68fd4eadc2ee6
kukita <ku.k669500@gmail.com>
2020-09-22 14:30:04 +0900

    リファクタ

--- app/src/main/java/kktyu/xyz/testphotoviewer/SearchFragment.kt
+++ app/src/main/java/kktyu/xyz/testphotoviewer/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kktyu.xyz.testphotoviewer.databinding.FragmentSearchBinding
+import kktyu.xyz.testphotoviewer.photo_list.PhotoListFragment
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
