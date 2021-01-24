1d24d7d2cb5e7385308b9db1c76223c89365818f
kktyu <ku.k669500@gmail.com>
2021-01-24 17:53:18 +0900

    ViewModelと役割分担をきちんとしてコードをきれいにしたよ

--- app/src/main/java/kktyu/xyz/testphotoviewer/ui/list/PhotoItem.kt
+++ app/src/main/java/kktyu/xyz/testphotoviewer/ui/list/PhotoItem.kt
@@ -1,33 +1,16 @@
 package kktyu.xyz.testphotoviewer.ui.list
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import com.xwray.groupie.databinding.BindableItem
 import kktyu.xyz.testphotoviewer.R
 import kktyu.xyz.testphotoviewer.databinding.PhotoItemBinding
-import kktyu.xyz.testphotoviewer.ui.detail.PhotoDetailFragment
+
+typealias OnSelectItem = (ListItem) -> Unit
 
 class PhotoItem(
-    private val photoModel: ListItem,
-    private val activity: FragmentActivity,
-    fragmentManager: FragmentManager?
+    private val photoModel: ListItem
 ) :
     BindableItem<PhotoItemBinding>() {
-    private val clickListener: (View) -> Unit = {
-        val bundle = Bundle()
-        bundle.putString(activity.getString(R.string.ID), photoModel.id)
-        bundle.putString(activity.getString(R.string.URL), photoModel.photo.url)
-
-        val fragment = PhotoDetailFragment()
-        fragment.arguments = bundle
-
-        fragmentManager!!.beginTransaction()
-            .add(R.id.container, fragment)
-            .addToBackStack("PhotoListFragment")
-            .commit()
-    }
+    var clickListener: OnSelectItem? = null
 
     override fun getLayout() = R.layout.photo_item
 
@@ -35,6 +18,6 @@ class PhotoItem(
         viewBinding.viewModel = ListItemModel()
         viewBinding.viewModel?.item = photoModel
         viewBinding.imageLoader = ListImageLoader
-        viewBinding.root.setOnClickListener(clickListener)
+        viewBinding.root.setOnClickListener { clickListener?.invoke(photoModel) }
     }
 }
